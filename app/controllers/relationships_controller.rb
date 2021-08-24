e5e7085bcc03a940b2b9ef540fd31864a670763c
nobe9216 <nobe9216@gmail.com>
2021-08-24 20:10:58 +0000

    [update]タイムライン画面実装

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -22,16 +22,26 @@ class RelationshipsController < ApplicationController
   def timeline
     user = current_user
     following_users = user.followings
-    following_users_id = []
-    following_users.each do |following_users|
-      following_users_id << following_users.id
-    end
+
+    # フォローしているユーザーのidだけ抽出して配列に加える
+    # following_users_id = []
+    # following_users.each do |following_users|
+    #   following_users_id << following_users.id
+    # end
+
+    # フォローしているユーザーのidと紐づく記録を抽出して配列に加える
     timeline = []
-    following_users_id.each do |following_user_id|
+    # following_users_id.each do |following_user_id|
+    following_users.ids.each do |following_user_id|
       # timeline_info = Status.where(user_id: following_user_id).order(date: :desc).limit(100)
       timeline_info = Status.where(user_id: following_user_id).limit(100)
-      timeline << timeline_info
+      # timeline << timeline_info
+      timeline += timeline_info
+      # byebug
     end
+    # byebug
+    # timeline = Hash[*timeline]
+    timeline = timeline.sort { |a, b| a[:date] <=> b[:date] }
     @timeline = timeline
     # byebug
   end

a7c6637f713ac29d7719158e6602f2bd12f8af3b
nobe9216 <nobe9216@gmail.com>
2021-08-25 19:24:25 +0000

    [update]いいね非同期化（途中）

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,17 +2,15 @@ class FavoritesController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    @user = User.find(params[:user_id])
-    @status = Status.find(params[:status_id])
-    favorite = current_user.favorites.new(status_id: @status.id)
+    status = Status.find(params[:status_id])
+    favorite = current_user.favorites.new(status_id: status.id)
     favorite.save
     # redirect_to request.referer
   end
 
   def destroy
-    @user = User.find(params[:user_id])
-    @status = Status.find(params[:status_id])
-    favorite = current_user.favorites.find_by(status_id: @status.id)
+    status = Status.find(params[:status_id])
+    favorite = current_user.favorites.find_by(status_id: status.id)
     favorite.destroy
     # redirect_to request.referer
   end

af9da48491ae852233b832411fc73c29525d594d
nobe9216 <nobe9216@gmail.com>
2021-08-29 12:03:23 +0000

    [fix]退会後の遷移先を変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -49,7 +49,7 @@ class UsersController < ApplicationController
   def destroy
     user = User.find(params[:id])
     user.destroy
-    redirect_to user_path(user.id)
+    redirect_to root_path
   end
 
   private

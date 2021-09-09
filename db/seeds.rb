1fb71eade1effe3706b5fa296cb2d5f7afb865ab
nobe9216 <nobe9216@gmail.com>
2021-09-09 11:42:50 +0000

    [update]seeds.rbの記述追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+30.times do |n|
+  gimei = Gimei.new
+  User.create(
+    email: "test#{n}@gmail.com",
+    password: "111111",
+    last_name: gimei.name.last.kanji,
+    last_name_kana: gimei.name.last.katakana,
+    first_name: gimei.name.first.kanji,
+    first_name_kana: gimei.name.first.katakana,
+    nickname: gimei.name.kanji,
+    start_weight: 60,
+    goal_weight: 55,
+    start_fat: 20,
+    age: 25,
+    height: 165,
+    self_introduction: "よろしくお願いします。"
+  )
+end
